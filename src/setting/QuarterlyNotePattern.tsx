76a65d8808967a435bddaa932a9b8e65cfc3db87
luzi <luzi_smile@163.com>
2024-03-28 22:46:41 +0800

    [UPDATE]优化设置界面显示的内容

--- src/setting/QuarterlyNotePattern.tsx
+++ src/setting/QuarterlyNotePattern.tsx
@@ -17,15 +17,18 @@ export default function QuarterlyNotePattern({mainController}: { mainController:
     return <>
         <div className="setting-item-info">
             <div className="setting-item-name">
-                文件名样式
+                文件命名规则
             </div>
             <div className="setting-item-description">
-                <div><a href="https://moment.github.io/luxon/#/formatting?id=table-of-tokens">语法说明</a></div>
-                <div>今天：{text}</div>
+                <div>请使用&nbsp;<a
+                    href="https://moment.github.io/luxon/#/formatting?id=table-of-tokens">luxon语法</a>&nbsp;指定笔记文件的生成路径
+                </div>
+                <div>规则应用后的文件路径为（基于当前日期）：{text}</div>
             </div>
         </div>
         <div className="setting-item-control">
-            <input type="text" value={pattern} spellCheck="false" onChange={onInputChange}/>
+            <input type="text" value={pattern} spellCheck="false" onChange={onInputChange}
+                   placeholder="季度总结/yyyy-qq"/>
         </div>
     </>
 }

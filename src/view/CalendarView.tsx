c60a0a82cfe6bfa9078dbb7b6e0302c8ff2c7f4b
dongjin <1163767451@qq.com>
2024-05-22 11:15:45 +0800

    优化开发步骤，初步使用antd替代

--- src/view/CalendarView.tsx
+++ src/view/CalendarView.tsx
@@ -1,70 +1,80 @@
-import {StrictMode} from "react";
-import {createRoot, Root} from "react-dom/client";
-import {Provider} from "react-redux";
-import {ItemView, WorkspaceLeaf} from "obsidian";
-import {MainControllerContext} from "../base/context";
-import {store} from "../redux/store";
-import CalendarViewImpl from "./calendar_view/CalendarViewImpl";
+import { StrictMode } from "react";
+import { createRoot, Root } from "react-dom/client";
+// import {Provider} from "react-redux";
+import { ItemView, WorkspaceLeaf } from "obsidian";
+import { MainControllerContext } from "../base/context";
+// import {store} from "../redux/store";
+// import CalendarViewImpl from "./calendar_view/CalendarViewImpl";
+import Calendar from "./component/Calendar";
 import MainController from "../core/MainController";
-
+import { ConfigProvider } from "antd";
+import zhCN from 'antd/locale/zh_CN';
 
 export const VIEW_TYPE_CALENDAR = "calendar-view";
 
-
 export class CalendarView extends ItemView {
+  private readonly mainController: MainController;
+  private root: Root | null = null;
+  theme: {};
 
-    private readonly mainController: MainController;
-    private root: Root | null = null;
+  constructor(leaf: WorkspaceLeaf, mainController: MainController) {
+    super(leaf);
+    this.mainController = mainController;
+    this.icon = "lucide-calendar-check";
+		// console.log(this.app.getTheme());
+    this.theme = {
+			token: {
+				colorPrimary: this.app.getAccentColor(),
+			},
+			components: {
+				Calendar: {
+					fullBg: 'transparent',
+					fullPanelBg: 'transparent',
+				},
+			},
+		};
+  }
 
-    constructor(leaf: WorkspaceLeaf, mainController: MainController) {
-        super(leaf);
-        this.mainController = mainController;
-        this.icon = "lucide-calendar-check";
+  // 视图类型
+  getViewType() {
+    return VIEW_TYPE_CALENDAR;
+  }
+
+  getDisplayText() {
+    return "日历";
+  }
+
+  // 打开时的初始化操作
+  async onOpen() {
+    this.root = createRoot(this.containerEl.children[1]);
+    this.root.render(
+      <StrictMode>
+        <MainControllerContext.Provider value={this.mainController}>
+          <ConfigProvider theme={this.theme} locale={zhCN}>
+            <Calendar />
+          </ConfigProvider>
+        </MainControllerContext.Provider>
+      </StrictMode>
+    );
+  }
+
+  public flush() {
+    if (this.root === null) {
+      return;
     }
+    this.root.render(
+      <StrictMode>
+        <MainControllerContext.Provider value={this.mainController}>
+          <ConfigProvider theme={this.theme} locale={zhCN}>
+            <Calendar />
+          </ConfigProvider>
+        </MainControllerContext.Provider>
+      </StrictMode>
+    );
+  }
 
-    // 视图类型
-    getViewType() {
-        return VIEW_TYPE_CALENDAR;
-    }
-
-    getDisplayText() {
-        return "日历";
-    }
-
-    // 打开时的初始化操作
-    async onOpen() {
-
-        console.log(this.getIcon());
-
-        this.root = createRoot(this.containerEl.children[1]);
-        this.root.render(
-            <StrictMode>
-                <MainControllerContext.Provider value={this.mainController}>
-                    <Provider store={store}>
-                        <CalendarViewImpl/>
-                    </Provider>
-                </MainControllerContext.Provider>
-            </StrictMode>,
-        );
-    }
-
-    public flush() {
-        if (this.root === null) {
-            return;
-        }
-        this.root.render(
-            <StrictMode>
-                <MainControllerContext.Provider value={this.mainController}>
-                    <Provider store={store}>
-                        <CalendarViewImpl/>
-                    </Provider>
-                </MainControllerContext.Provider>
-            </StrictMode>,
-        );
-    }
-
-    // 关闭时的资源释放操作
-    async onClose() {
-        this.root?.unmount();
-    }
+  // 关闭时的资源释放操作
+  async onClose() {
+    this.root?.unmount();
+  }
 }

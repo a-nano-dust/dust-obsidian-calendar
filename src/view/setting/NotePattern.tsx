b05c940e25e49f6fb6fc14fe2cdecc630863bd28
a-nano-dust <nano_dust@163.com>
2024-06-06 00:54:58 +0800

    [FIX]修复文件命名规则描述中 luxon 链接无法打开的问题；在文件命名规则描述中添加关于转义的说明

--- src/view/setting/NotePattern.tsx
+++ src/view/setting/NotePattern.tsx
@@ -22,12 +22,14 @@ export default function NotePattern({plugin, noteType}: { plugin: DustCalendarPl
             </div>
             <div className="setting-item-description">
                 <div>请使用&nbsp;<a
-                    href="src/view/setting/DailyNotePattern#/formatting?id=table-of-tokens">luxon语法</a>&nbsp;指定笔记文件的生成路径。
+                    href="https://moment.github.io/luxon/#/formatting?id=table-of-tokens">luxon语法</a>&nbsp;指定笔记文件的生成路径。
                 </div>
                 {notePattern.length === 0
                     ? <div className="d-color-error">尚未配置文件命名规则，Dust Calendar 将无法为您创建笔记文件。</div>
-                    : <div>规则应用后的文件路径为（基于当前日期）：{text}</div>
+                    : <div>规则应用后的文件路径为（基于当前日期）：<b className="d-color-accent">{text}</b></div>
                 }
+                <div><b>注意：</b>使用单引号可以进行转义，例如：yyyy 'year' MM 'month' dd 'day'
+                    格式化结果为 {DateTime.now().toFormat("yyyy 'year' MM 'month' dd 'day'")}</div>
             </div>
         </div>
         <div className="setting-item-control">
